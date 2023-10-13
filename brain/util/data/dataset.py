3a85ae7af3efd615307214cfb0678804b9fa2a13
salimnamvar <salim.namvar@gmail.com>
2023-10-13 20:38:21 +0400

    feat(Dataset): The dataset class has been updated to correctly handle the sequence of RNA for BERT based models and nan values in the reactivity vector which can affect the attention mask.

--- brain/util/data/dataset.py
+++ brain/util/data/dataset.py
@@ -4,12 +4,13 @@
 
 
 # region Imported Dependencies
-import time
 from typing import List, Tuple
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
 import pyarrow.parquet as pq
-from torchvision import transforms
+from transformers import BertTokenizer
 from brain.util.data.scheme import DatasetScheme, SampleIndex, SampleIndices
 # endregion Imported Dependencies
 
@@ -19,17 +20,16 @@ class SRRFCDataset(Dataset):
 
     """
 
-    def __init__(self, a_file: str, a_chunk_size: int, a_seq_cats: List[str] = ['A', 'C', 'G', 'U']):
+    def __init__(self, a_file: str, a_chunk_size: int, a_max_length: int = 457):
         self.file: str = a_file
         self.chunk_size: int = a_chunk_size
-        self.seq_cats: List[str] = a_seq_cats
-        self.seq_cat_to_index: dict = {category: index for index, category in enumerate(self.seq_cats)}
+        self.max_length: int = a_max_length
         self.table = pq.read_table(self.file)
         self.chunks = self.table.to_batches(self.chunk_size)
         self.sample_indices = SampleIndices()
         self.__init_samples()
         self.dataset_scheme: DatasetScheme = DatasetScheme(a_feat=self.chunks[0].to_pandas().columns.tolist())
-        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
     def __init_samples(self):
         sample_counter = 0
@@ -42,21 +42,30 @@ class SRRFCDataset(Dataset):
     def __len__(self):
         return self.table.num_rows
 
-    def __preprocess(self, a_sequence: str, a_reactivity: List[float]) -> Tuple[np.ndarray, np.ndarray]:
-        sequence = self.__proc_sequence(a_sequence)
-        reactivity = self.__proc_reactivity(a_reactivity, a_seq_len=len(a_sequence))
-        return sequence, reactivity
+    def __preprocess(self, a_sequence: str, a_reactivity: List[float]) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+        reactivity = torch.tensor(np.array(a_reactivity[:len(a_sequence)], dtype=float))
 
-    def __proc_sequence(self, a_seq: str) -> np.ndarray:
-        # Convert the sequence to an array of category indices
-        seq_indices = np.array([self.seq_cat_to_index[cat] for cat in a_seq], dtype=int)
+        # Sequence Tokenization
+        sequence = ' '.join(a_sequence)
+        inputs = self.tokenizer.encode_plus(text=sequence, text_pair=None, add_special_tokens=True,
+                                            max_length=self.max_length,
+                                            padding='max_length', return_token_type_ids=True,
+                                            truncation=True)
+        input_ids = torch.tensor(inputs['input_ids'], dtype=torch.long)
+        attention_mask = torch.tensor(inputs['attention_mask'], dtype=torch.long)
+        token_type_ids = torch.tensor(inputs["token_type_ids"], dtype=torch.long)
 
-        # Create a one-hot encoding array
-        one_hot_seq = (seq_indices[:, None] == np.arange(len(self.seq_cats))).astype(int)
-        return one_hot_seq
+        # Reactivity Nan values
+        nan_ids = torch.where(torch.isnan(reactivity))
+        # Plus one index more for passing the classification token which is in the index 0
+        attention_mask[nan_ids[0] + 1] = 0
+        reactivity[nan_ids] = 0
 
-    def __proc_reactivity(self, a_react: List[float], a_seq_len: int) -> np.ndarray:
-        return np.array(a_react[:a_seq_len], dtype=float)
+        # Reactivity Padding
+        padded_reactivity = torch.zeros(self.max_length)
+        padded_reactivity[:len(reactivity)] = reactivity
+
+        return input_ids, attention_mask, token_type_ids, padded_reactivity
 
     def __get_sample(self, a_index: int) -> Tuple[str, List[float]]:
         smp = self.sample_indices[a_index]
@@ -65,25 +74,22 @@ class SRRFCDataset(Dataset):
                       self.dataset_scheme.label]
         return sequence, reactivity
 
-    def __getitem__(self, a_index):
+    def __getitem__(self, a_index) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
         try:
             # Get sample
             sequence, reactivity = self.__get_sample(a_index)
 
             # Preprocess sample
-            sequence, reactivity = self.__preprocess(sequence, reactivity)
+            input_ids, attention_mask, token_type_ids, reactivity = self.__preprocess(sequence, reactivity)
         except Exception as e:
             raise e
-        return sequence, reactivity
+        return input_ids, attention_mask, token_type_ids, reactivity
 
 
 if __name__ == '__main__':
     if False:
-        t1 = time.time()
         dataset = SRRFCDataset(a_file='G:/Challenges/RNA/data/train_data.parquet', a_chunk_size=1000)
         dataloader = DataLoader(dataset=dataset, batch_size=1000, shuffle=True,
                                 collate_fn=lambda batch: tuple(zip(*batch)))
-        for i, (x_data, y_data) in enumerate(dataloader):
+        for i, (input_ids, attention_mask, token_type_ids, reactivity) in enumerate(dataloader):
             print(f'Data Sample Index: {i}')
-        t2 = time.time()
-        print(f'Dataloader time: {t2 - t1}')
