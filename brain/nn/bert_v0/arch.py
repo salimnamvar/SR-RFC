b35aca7e886c9da72430fab892444460d6a0154c
salimnamvar <salim.namvar@gmail.com>
2023-10-14 18:51:56 +0400

    refactor(BERT): The Bert arch_0 is updated.

--- brain/nn/bert_v0/arch.py
+++ brain/nn/bert_v0/arch.py
@@ -4,19 +4,21 @@
 
 
 # region Imported Dependencies
+import torch
 from torch import nn
 from transformers import BertModel
 # endregion Imported Dependencies
 
 
-class Bert(nn.Module):
+class Arch(nn.Module):
     def __init__(self, a_max_length: int = 457) -> None:
         super().__init__()
         self.bert_layer: BertModel = BertModel.from_pretrained('bert-base-uncased')
-        self.dropout_layer: nn.Dropout = nn.Dropout(0.3)
+        self.dropout_layer: nn.Dropout = nn.Dropout(0.1)
         self.reactivity_layer: nn.Linear = nn.Linear(768, a_max_length)
 
-    def forward(self, a_input_ids, a_attention_mask, a_token_type_ids):
+    def forward(self, a_input_ids: torch.Tensor, a_attention_mask: torch.Tensor,
+                a_token_type_ids: torch.Tensor) -> torch.Tensor:
         _, output1 = self.bert_layer(input_ids=a_input_ids, attention_mask=a_attention_mask,
                                      token_type_ids=a_token_type_ids, return_dict=False)
         output2 = self.dropout_layer(output1)
