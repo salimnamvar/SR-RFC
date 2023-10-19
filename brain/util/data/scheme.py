7ecfd63073745198333ad4a78423524b1de15147
salimnamvar <salim.namvar@gmail.com>
2023-10-20 00:00:10 +0400

    feat(dataset): dataset scheme is updated by adding name of columns.

--- brain/util/data/scheme.py
+++ brain/util/data/scheme.py
@@ -2,24 +2,32 @@
 
 """
 
-
 # region Imported Dependencies
 from dataclasses import dataclass
 from typing import List
+
+
 # endregion Imported Dependencies
+
+
+@dataclass
+class Column:
+    name: str
+    index: int
 
 
 @dataclass
 class DatasetScheme:
     features: List[str]
-    label: List[int]
-    input: int
+    label: List[Column]
+    input: Column
 
     def __init__(self, a_feat: List[str]) -> None:
         self.features = a_feat
-        self.label = [i for i, col in enumerate(self.features) if 'reactivity' in col and 'error' not in col]
-        self.input = self.features.index('sequence')
-        self.experiment = self.features.index('experiment_type')
+        self.label = [Column(name=col, index=i) for i, col in enumerate(self.features) if
+                      'reactivity' in col and 'error' not in col]
+        self.input = Column(name='sequence', index=self.features.index('sequence'))
+        self.experiment = Column(name='experiment_type', index=self.features.index('experiment_type'))
 
 
 @dataclass
