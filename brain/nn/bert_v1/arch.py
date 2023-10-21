f8929ac37da014cce8aff7c3c59dc558a232c3c0
salimnamvar <salim.namvar@gmail.com>
2023-10-21 21:40:18 +0400

    feat(BERT): The architecture of bert v.1 is updated to be smaller.

--- brain/nn/bert_v1/arch.py
+++ brain/nn/bert_v1/arch.py
@@ -5,7 +5,7 @@
 # region Imported Dependencies
 import torch
 from torch import nn
-from transformers import BertModel
+from transformers import BertModel, BertConfig
 
 
 # endregion Imported Dependencies
@@ -14,13 +14,13 @@ from transformers import BertModel
 class Arch(nn.Module):
     def __init__(self, a_max_length: int = 457) -> None:
         super().__init__()
-        self.bert_layer: BertModel = BertModel.from_pretrained('bert-base-uncased')
+        config = BertConfig(vocab_size=5, num_hidden_layers=6, num_attention_heads=6, hidden_size=768, )
+        self.bert_layer: BertModel = BertModel(config)
         self.reactivity_layer: nn.Linear = nn.Linear(768, a_max_length)
-        self.sigmoid_layer: nn.Sigmoid = nn.Sigmoid()
 
     def forward(self, a_input_ids: torch.Tensor, a_attention_mask: torch.Tensor,
                 a_token_type_ids: torch.Tensor) -> torch.Tensor:
         _, output = self.bert_layer(input_ids=a_input_ids, attention_mask=a_attention_mask,
-                                     token_type_ids=a_token_type_ids, return_dict=False)
+                                    token_type_ids=a_token_type_ids, return_dict=False)
         output = self.reactivity_layer(output)
         return output
