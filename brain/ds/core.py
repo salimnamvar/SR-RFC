aafe91c56fd07768df09f5f11f5560c68ab28da0
Salim Namvar <salim.namvar@gmail.com>
2023-11-04 19:18:38 +0400

    feat(dataset): The dataset core is coded to select any dataset handler for loading and preprocessing data.

---
+++ brain/ds/core.py
@@ -0,0 +1,36 @@
+""" Core Dataset
+
+"""
+
+
+# region Imported Dependencies
+import importlib
+from typing import Iterable
+import torch
+from brain.ds.hdl.base import BaseDataset
+from brain.util.base.arg import Param
+# endregion Imported Dependencies
+
+
+class CoreDataset(BaseDataset):
+    def __init__(self, a_name: str, a_ds: Param) -> None:
+        self.name: str = a_name
+        self.ds_param: Param = a_ds
+        self.ds: BaseDataset = None
+        self.__init_ds()
+
+    def __init_ds(self):
+        # Build Dataset
+        try:
+            module_name = f"brain.ds.hdl.{self.ds_param.name}"
+            module = importlib.import_module(module_name)
+            arch_class = getattr(module, 'Dataset')
+        except (AttributeError, ModuleNotFoundError):
+            raise ValueError('Invalid `dataset.name` is entered.')
+        self.ds: BaseDataset = arch_class(**self.ds_param.kwargs)
+
+    def __len__(self) -> int:
+        return len(self.ds)
+
+    def __getitem__(self, a_index) -> Iterable[torch.Tensor]:
+        return self.ds[a_index]
