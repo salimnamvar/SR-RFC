5ac856076c7945009a48c88c47c1b0823df16c24
Salim Namvar <salim.namvar@gmail.com>
2023-11-14 19:45:02 +0400

    chore(tool): The tool of splitting data samples is updated to exclude the NaN reactivity vectors from sampling.

--- tool/sample/split_class_samples.py
+++ tool/sample/split_class_samples.py
@@ -1,13 +1,26 @@
+""" Split Class Data Sample
+
+"""
+
+# region Imported Dependencies
 import pandas as pd
 from sklearn.model_selection import train_test_split
+# endregion Imported Dependencies
 
-train = 'G:/Challenges/RNA/data/train_data.parquet'
+# Load the CSV file containing IDs to exclude
+exclude_file = 'G:/Challenges/RNA/code/SR-RFC/data/nan_samples.csv'
+exclude_df = pd.read_csv(exclude_file, header=None, names=['sequence_id'])
 
-df = pd.read_parquet(train)
+# Load the original data from the Parquet file
+train_file = 'G:/Challenges/RNA/data/train_data.parquet'
+df = pd.read_parquet(train_file)
 
 # Extract the first and third columns (0-based index)
 df.iloc[:, 0] = df.index
 selected_columns = df.iloc[:, [0, 2]]
+
+# Filter out samples marked for exclusion
+selected_columns = selected_columns[~selected_columns['sequence_id'].isin(exclude_df['sequence_id'])]
 
 # Split the DataFrame into two separate DataFrames based on the 'experiment_type' column
 df_2A3_MaP = selected_columns[selected_columns['experiment_type'] == '2A3_MaP']
@@ -18,8 +31,8 @@ df_2A3_MaP = df_2A3_MaP.sample(frac=1, random_state=42)
 df_DMS_MaP = df_DMS_MaP.sample(frac=1, random_state=42)
 
 # Split each DataFrame into train and validation sets
-train_2A3_MaP, val_2A3_MaP = train_test_split(df_2A3_MaP, test_size=0.3, random_state=42)
-train_DMS_MaP, val_DMS_MaP = train_test_split(df_DMS_MaP, test_size=0.3, random_state=42)
+train_2A3_MaP, val_2A3_MaP = train_test_split(df_2A3_MaP, test_size=0.2, random_state=42)
+train_DMS_MaP, val_DMS_MaP = train_test_split(df_DMS_MaP, test_size=0.2, random_state=42)
 
 # Save the training and validation sets as separate CSV files without headers
 train_2A3_MaP[['sequence_id']].to_csv('train_2A3_MaP.csv', index=False, header=False)
