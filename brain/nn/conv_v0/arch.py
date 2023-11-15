cb8fecf0e15ff5c4463efab599c920357735c4df
Salim Namvar <salim.namvar@gmail.com>
2023-11-15 23:17:35 +0400

    feat(lstm): The lstm model is also integrated and ready for expanding.

--- brain/nn/conv_v0/arch.py
+++ brain/nn/conv_v0/arch.py
@@ -16,28 +16,28 @@ class Arch(nn.Module):
 
         # Define convolutional layers
         self.layers = nn.Sequential(
-            nn.Conv2d(1, 484, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(484),
+            nn.Conv2d(1, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(484, 242, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(242),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(242, 120, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(120),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(120, 60, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(60),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(60, 30, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(30),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(30, 1, kernel_size=1),
-            nn.Sigmoid()
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
+
+            nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.BatchNorm2d(64),
+
+            nn.Conv2d(64, 1, kernel_size=(3, 3), padding=(1, 1)),
         )
 
     def forward(self, x: Tensor) -> Tensor:
